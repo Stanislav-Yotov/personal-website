090170d3f78294e883a12e9ed7bfddeb2c4183dd
Stanislav Yotov <stanislav.iotov@abv.bg>
2022-09-17 15:05:26 +0300

    proceeding with the about page

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -2,7 +2,16 @@ import styles from './About.module.css';
 
 function AboutPage(props) {
     return (
-        <div className={styles.container}>About</div>
+        <div className={styles.container}>
+            <div className={styles.greet}>
+                Hi. I am Stanislav Yotov. Nice to meet you!
+            </div>
+            <div className={styles.introduce}>
+                I am passionate about building software which makes people's life easier.
+                I am curious in terms of technology and every day is a challenge to learn something new,
+                and become a better version of myself.
+            </div>
+        </div>
     );
 }
 
