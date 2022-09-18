90795b877986281913e947755e908dad9715f93c
Stanislav Yotov <stanislav.iotov@abv.bg>
2022-09-18 14:42:50 +0300

    starting with the skills page

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,8 +1,19 @@
 import styles from './Skills.module.css';
+import html from '../../assets/html.png';
+import css from '../../assets/css.png';
+import javascript from '../../assets/javascript.png';
+import react from '../../assets/react.png';
+import node from '../../assets/node.png';
+import github from '../../assets/github.png';
 
 function Skills(props) {
     return (
-        <div className={styles.container}>Skills</div>
+        <div className={styles.container}>
+            <div>
+                <p>Skills</p>
+                <p>// These are the technologies I am working with</p>
+            </div>
+        </div>
     );
 }
 
