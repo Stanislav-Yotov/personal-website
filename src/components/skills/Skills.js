cf758a2e76334bd783c757161bcf23dcb81960fe
Stanislav Yotov <stanislav.iotov@abv.bg>
2022-09-18 16:34:54 +0300

    finished with the skills page

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -10,8 +10,34 @@ function Skills(props) {
     return (
         <div className={styles.container}>
             <div>
-                <p>Skills</p>
-                <p>// These are the technologies I am working with</p>
+                <p><strong>Skills</strong></p>
+                <p>// These are the technologies I am working with //</p>
+            </div>
+            <div className={styles.icons}>
+                <div>
+                    <img src={html} alt='html icon' />
+                    <p>HTML</p>
+                </div>
+                <div>
+                    <img src={css} alt='css icon' />
+                    <p>CSS</p>
+                </div>
+                <div>
+                    <img src={javascript} alt='js icon' />
+                    <p>JavaScript</p>
+                </div>
+                <div>
+                    <img src={react} alt='react icon' />
+                    <p>React</p>
+                </div>
+                <div>
+                    <img src={node} alt='node icon' />
+                    <p>Node js</p>
+                </div>
+                <div>
+                    <img src={github} alt='github icon' />
+                    <p>Github</p>
+                </div>
             </div>
         </div>
     );
