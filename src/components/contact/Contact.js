794d474674e5e87fe6de863525377b94b0eeab0c
Stanislav Yotov <stanislav.iotov@abv.bg>
2022-09-24 07:47:22 +0300

    adding contact form

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -2,7 +2,22 @@ import styles from './Contact.module.css';
 
 function Contact(props) {
     return (
-        <div className={styles.container}>Contact</div>
+        <div className={styles.container}>
+            <div className={styles.contactForm}>
+                <form>
+                    <label htmlFor='fullName'>Full name</label>
+                    <input type='text' id='fname' name='fullname' placeholder='Your full name' />
+
+                    <label htmlFor='email'>Email</label>
+                    <input type='text' id='email' name='email' placeholder='Your email adress' />
+
+                    <label htmlFor='message'>Send me a message</label>
+                    <textarea id="subject" name="subject" placeholder="Type your message here" />
+
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
+        </div>
     );
 }
 
